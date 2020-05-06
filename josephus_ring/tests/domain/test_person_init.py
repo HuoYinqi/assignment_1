88f598fbb5a8dfd9cf189d11f4ce6f5ec92d99bd
HuoYinqi <983673234@qq.com>
2020-05-06 14:34:35 +0800

    将person.py移到domain文件夹 并更改测试文件中到入行

--- josephus_ring/tests/domain/test_person_init.py
+++ josephus_ring/tests/domain/test_person_init.py
@@ -1,6 +1,6 @@
 import pytest
 
-from josephus_ring.shared.person import Person
+from josephus_ring.domain.person import Person
 
 def test_person_init():
     someone = Person('Bob', 12)
@@ -17,4 +17,17 @@ def test_person_value_of_age_is_zero():
 
     assert someone.name == 'Bob'
     assert someone.age == 0
-    
+
+def test_person_from_dict():
+    adict = {'name': 'Bob', 'age': 12}
+    someone = Person.from_dict(adict)
+
+    assert someone.name == 'Bob'
+    assert someone.age == 12
+
+def test_person_to_dict():
+    someone = Person('Bob', 12)
+    result = someone.to_dict()
+
+    assert result == {'name': 'Bob', 'age': 12}
+
