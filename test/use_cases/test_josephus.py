3ee019da3b925c7b59e5a460b13065fdd11bda9f
HuoYinqi <983673234@qq.com>
2020-05-25 20:31:43 +0800

    fixed to test

--- test/use_cases/test_josephus.py
+++ test/use_cases/test_josephus.py
@@ -1,11 +1,29 @@
 import pytest
 
+from unittest import mock
 from collections.abc import Iterable
 
 from josephus.use_cases.josephus import Josephus
 from josephus.domain.person import Person
 
-def test_josephus_init():
+@pytest.fixture
+def people_example():
+    return [Person('Bob', 12), Person('Jack', 13), Person('Peter', 14)]
+    
+def test_josephus_init_with_reader():
+    reader = mock.MagicMock()
+    jos = Josephus(reader)
+
+    reader.__iter__.assert_called_with()
+    assert jos.start == 1
+    assert jos.step == 1
+
+def test_josephus_init_with_invalid_reader():
+    reader = mock.Mock()
+    with pytest.raises(TypeError):
+        Josephus(reader)
+
+def test_josephus_init_without_reader():
     jos = Josephus()
 
     assert jos.start == 1
@@ -19,62 +37,41 @@ def test_josephus_append():
     
     assert jos.people == [someone1]
 
-def test_josephus_pop():
-    someone1: Person = Person('Bob', 12)
-    someone2: Person = Person('Jack', 13)
-    someone3: Person = Person('Peter', 14)
+def test_josephus_pop(people_example):
     jos = Josephus()
-    jos.people = [someone1, someone2, someone3]
+    jos.people = people_example
     jos.pop(1)
 
-    assert jos.people == [someone1, someone3]
+    assert jos.people == [Person('Bob', 12), Person('Peter', 14)]
     
-def test_josephus_query_list():
-    someone1: Person = Person('Bob', 12)
-    someone2: Person = Person('Jack', 13)
-    someone3: Person = Person('Peter', 14)
+def test_josephus_query_list(people_example):
     jos = Josephus()
     jos.start = 2
     jos.step = 2
-    jos.people = [someone1, someone2, someone3]
+    jos.people = people_example
     result = jos.query_list()
 
-    assert result == [someone3, someone2, someone1]
+    assert result == [Person('Peter', 14), Person('Jack', 13), Person('Bob', 12)]
 
 def test_josephus_is_iterable():
     jos = Josephus()
 
     assert isinstance(jos, Iterable)
 
-def test_josephus_output_order_by_next():
-    someone1: Person = Person('Bob', 12)
-    someone2: Person = Person('Jack', 13)
-    someone3: Person = Person('Peter', 14)
+def test_josephus_output_order_by_next(people_example):
     jos = Josephus()
     jos.start = 2
     jos.step = 2
-    jos.people = [someone1, someone2, someone3]
+    jos.people = people_example
 
-    assert next(jos) == someone3
-    assert next(jos) == someone2
-    assert next(jos) == someone1
+    assert next(jos) == Person('Peter', 14)
+    assert next(jos) == Person('Jack', 13)
+    assert next(jos) == Person('Bob', 12)
     with pytest.raises(StopIteration):
         next(jos)
-    
-def test_josephus_init_with_reader():
-    someone1: Person = Person('Bob', 12)
-    someone2: Person = Person('Jack', 13)
-    someone3: Person = Person('Peter', 14)
-    persons = [someone1, someone2,someone3]
-    jos = Josephus(reader=persons)
 
-    assert jos.people == [someone1, someone2, someone3]
-
-def test_josephus_len():
-    someone1: Person = Person('Bob', 12)
-    someone2: Person = Person('Jack', 13)
-    someone3: Person = Person('Peter', 14)
+def test_josephus_len(people_example):
     jos = Josephus()
-    jos.people = [someone1, someone2, someone3]
+    jos.people = people_example
 
     assert len(jos) == 3
