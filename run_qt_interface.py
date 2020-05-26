32a93e898b99c0ccdf34705b287f67a98f378fcf
HuoYinqi <983673234@qq.com>
2020-05-26 14:24:49 +0800

    修改文件名

--- run_qt_interface.py
+++ run_qt_interface.py
@@ -1,7 +1,7 @@
 import sys
 from PyQt5 import QtWidgets
 
-from josephus.adapter.graphfic_interface import JosephusWindow
+from josephus.adapter.qt_interface import JosephusWindow
 
 if __name__ == '__main__':
     app = QtWidgets.QApplication(sys.argv)
